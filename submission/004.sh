9c3abb6cb7ffb88924a0b7657b1ecef206022155
Guilherme Terriaga <g.terriaga@wisetrust.ai>
2024-11-24 00:14:04 -0300

    004

--- submission/004.sh
+++ submission/004.sh
@@ -1,3 +1,3 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
-bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)#q4q7qurj" "[100,100]"
+bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)#q4q7qurj" "[100,100]" | jq -r
